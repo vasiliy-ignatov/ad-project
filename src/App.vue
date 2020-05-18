907aeea098aa7a74de76e170fd53f7c25a41a26f
v_ignatov <inaburat@gmail.com>
2020-05-18 20:41:57 +0300

    icons and nav-bar is added

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,34 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+	<v-app>
+		<v-navigation-drawer app temporary></v-navigation-drawer>
+
+		<v-app-bar app>
+			<v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+			<v-toolbar-title>Ad application</v-toolbar-title>
+
+			<v-spacer></v-spacer>
+
+			<v-toolbar-items>
+				<v-btn>Link one</v-btn>
+			</v-toolbar-items>
+		</v-app-bar>
+
+		<v-content>
+			<h1>Hello</h1>
+		</v-content>
+	</v-app>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+	data () {
+		return {
+			msg: 'Welcome to Your Vue.js App'
+		}
+	}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify' // path to vuetify export
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 
 new Vue({
-  vuetify,
-  el: '#app',
-  render: h => h(App)
+	vuetify,
+	el: '#app',
+	render: h => h(App)
 })
