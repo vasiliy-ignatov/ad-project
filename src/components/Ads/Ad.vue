89a0599dfc45a923d4c988abc3209808daf3ea6b
v_ignatov <inaburat@gmail.com>
2020-05-22 19:03:45 +0300

    my ad page

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -1,10 +1,23 @@
 <template>
 	<v-container>
-		<v-layout row>
-			<v-flex xs12>
-				<h1>Ad</h1>
-			</v-flex>
-		</v-layout>
+		<v-row
+			align="center"
+			justify="center"
+		>
+			<v-col cols="12" md="8">
+				<v-card>
+					<v-img src="https://picsum.photos/1700/1200?random" height="300px"></v-img>
+					<v-card-text>
+						<h1 class="text--primary">Lorem</h1>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, quos.</p>
+					</v-card-text>
+					<div class="d-flex justify-end pr-4 pb-4">
+						<v-btn class="mr-2" outlined color="warning">Edit</v-btn>
+						<v-btn class="success">Buy</v-btn>
+					</div>
+				</v-card>
+			</v-col>
+		</v-row>
 	</v-container>
 </template>
 
